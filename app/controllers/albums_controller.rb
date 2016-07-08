3d02ce3e0d1782ffdbd383d74f96850e2d10138a
DAlgma <dianx93@gmail.com>
2016-07-08 13:18:06 +0300

    albumite kustutamine

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -35,6 +35,13 @@ class AlbumsController < ApplicationController
 		end
 	end
 	
+	def destroy
+		@album = Album.find(params[:id])
+		@album.destroy
+		
+		redirect_to albums_path
+	end
+	
 	private
 	def album_params
 		params.require(:album).permit(:author, :name)
